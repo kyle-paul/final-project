462c00e8a7ad04440ff42a32359f8c59c7801ae0
Paul Delta <117391498+DeltaPaul4@users.noreply.github.com>
2023-05-10 01:01:46 +0700

    create_canvas function

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 from sidebar import create_sidebar
 from translator_panel import translator
 from spelling_panel import create_dictionary_function
+from keras.models import load_model, create_canvas, check
+
+# load model
+model = load_model('CNN_model.h5')
 
 # Create sidebar
 with st.sidebar:
@@ -14,6 +18,7 @@ if choice == "Translator":
 # panel 2: Spelling
 if choice == "spelling":
     st.title("Enhance your spelling")
-    create_dictionary_function()
+    result_str = create_dictionary_function()
+    create_canvas(model)
     
     
--- spelling_panel.py
+++ spelling_panel.py
@@ -1,5 +1,9 @@
 import streamlit as st
 from word_scrapping import word_scrapping
+import numpy as np
+from streamlit_drawable_canvas import st_canvas
+import cv2
+import skimage.transform
 
 def create_dictionary_function():
     if "score" not in st.session_state:
@@ -25,3 +29,38 @@ def create_dictionary_function():
     score = int(st.session_state["score"])
     count = int(st.session_state["count"])
     st.info(f"Your current score: {score - count} points")
+    
+def create_canvas(model):
+    stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 10)
+    realtime_update = st.sidebar.checkbox("Update in realtime", True)
+
+    result_str = ""
+    num_cols = st.number_input ('Enter the number of the word length', min_value=7, max_value=12, value=8, step=1)
+    cols = st.columns(num_cols)
+    
+    for i, col in enumerate (cols):
+        # Create a canvas component
+        canvas_result = st_canvas(
+            fill_color="rgba(255, 165, 0, 0.3)",  
+            stroke_width=stroke_width,
+            stroke_color="#ffffff",
+            background_color= "#000000",
+            update_streamlit=realtime_update,
+            drawing_mode="freedraw",
+            height=150,
+            width=150,
+            key="canvas" + str(i),
+        )
+        if canvas_result.image_data is not None:
+            gray = cv2.cvtColor(canvas_result.image_data, cv2.COLOR_BGR2GRAY)
+            resized = skimage.transform.resize(gray, output_shape=(28, 28))
+            input = resized.reshape((1,28,28,1))
+            prediction = model.predict(input)
+            label = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'] 
+            st.write(label[np.argmax(prediction)])
+            result_str += label[np.argmax(prediction)]
+            
+    return result_str
+            
+def check():
+    pass
